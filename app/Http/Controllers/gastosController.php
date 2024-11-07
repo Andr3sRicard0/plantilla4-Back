ac25d7433f5fd7c812c75e3fdad651621cee6071
Andr3sRicard0 <ricardoandrescorreo@gmail.com>
2024-11-07 10:53:29 -0500

    Apis de Gastos creadas

--- app/Http/Controllers/gastosController.php
+++ app/Http/Controllers/gastosController.php
@@ -4,20 +4,25 @@ namespace App\Http\Controllers;
 
 use App\Models\Gastos;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+use function Laravel\Prompts\error;
 
 class gastosController extends Controller
 {
+    //llamar a todos los datos
     public function index(){
         $gastos = Gastos::all();
         if($gastos->isEmpty()){
             $data = [
-                'message' => 'No hay gastos en la bd',
+                'message' => 'No hay datos en la bd',
                 'status' => 200
             ];
             return response()->json($data, 200);
         }
         return response()->json($gastos, 200);
     }
+    //llamar a los datos por id
     public function show($id){
         $gastos = Gastos::find($id);
         if(!$gastos){
@@ -29,6 +34,95 @@ class gastosController extends Controller
         }
         $data = [
             'gasto' => $gastos,
+            'status' => 200
+        ];
+        return response()->json($data, 200);
+    }
+    //crear datos
+    public function store(Request $request){
+        $validator = Validator::make($request->all(),[
+            'descripcion' => 'required|string|max:255',
+            'costo' => 'required|numeric'
+        ]);
+        //verificar que la data que se envia no tiene algun tipo de error
+        if($validator->fails()){
+            $data = [
+                'message' => 'Error en envío de la data gastos',
+                'errors ' => $validator->errors(),
+                'status' => 400
+            ];
+            return response()->json($data, 400);
+        }
+        
+        $gastos = Gastos::create([
+            'descripcion' => $request->descripcion,
+            'costo' => $request->costo
+        ]);
+        //verificar si la creacion se hizo de manera satisfactoria
+        if(!$gastos){
+            $data = [
+                'message' => 'Error al crear la data gastos',
+                'status' => 500
+            ];
+            return response()->json($data, 500);
+        }
+        //repuesta de creacion exitosa
+        $data = [
+            'message' => 'Creación exitosa de la data gastos',
+            'gasto' => $gastos,
+            'status' => 201
+        ];
+        return response()->json($data, 201);
+    }
+    //actualizar un dato
+    public function update(Request $request, $id){
+        $gastos = Gastos::find($id);
+        if(!$gastos){
+            $data = [
+                'message' => 'Gasto no encontrado',
+                'status' => 404
+            ];
+            return response()->json($data, 404);
+        }
+        $validator = Validator::make($request->all(), [
+            'descripcion' => 'required|string|max:255',
+            'costo' => 'required|numeric'
+        ]);
+        //condicional si falla el validator de la data
+        if($validator->fails()){
+            $data = [
+                'message' => 'Error al ingreso de la data de gastos',
+                'status' => 500
+            ];
+            return response()->json($data, 500);
+        }
+        $gastos->descripcion = $request->descripcion;
+        $gastos->costo = $request->costo;
+        
+        $gastos->save();
+
+        $data = [
+            'message' => 'Data de gasto creada de manera exitosa',
+            'gasto' => $gastos,
+            'status' => 200
+        ];
+        return response()->json($data, 200);
+    }
+    //funcion para eliminar data
+    public function destroy($id){
+        $gastos = Gastos::find($id);
+        //Error al no encontrar la data
+        if(!$gastos){
+            $data = [
+                'message' => 'Error al encontrar la data gasto',
+                'status' => 404
+            ];
+            return response()->json($data, 404);
+        }
+
+        $gastos->delete();
+        $data = [
+            'message' => 'Dato de gasto eliminado de manera correcta',
             'status' => 200
         ];
         return response()->json($data, 200);
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\gastosController;
 use App\Http\Controllers\studentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -11,3 +12,9 @@ Route::post('/estudiante', [studentController::class, 'store']);
 Route::put('/estudiante/{id}', [studentController::class, 'update']);
 Route::delete('/estudiante/{id}', [studentController::class, 'destroy']);
 
+//gastos api
+Route::get('/gasto', [gastosController::class, 'index']);
+Route::get('/gasto/{id}', [gastosController::class, 'show']);
+Route::post('gasto', [gastosController::class, 'store']);
+Route::put('gasto/{id}', [gastosController::class, 'update']);
+Route::delete('gasto/{id}', [gastosController::class, 'destroy']);
